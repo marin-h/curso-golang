18108c7aa37a36455aa5f8dc0a81559692d512b5
marin-h <marinahuberman@gmail.com>
2020-07-03 10:03:00 -0300

    nicer solution, using nested structs

--- clase3/main.go
+++ clase3/main.go
@@ -5,35 +5,40 @@ import (
 )
 
 type Cart struct {
-	List map[Product]float32
+	List []Item
 }
 
 func (cart Cart) Checkout() {
 	fmt.Println("== CHECKOUT ==")
 	var total float32 
-	for product, quantity := range cart.List {
-		fmt.Println(product.Name, ":", quantity)
-		total += product.Total(quantity)
+	for _, item := range cart.List {
+		fmt.Println(item.Product.Name, ":", item.Quantity)
+		total += item.Total()
 	}
 	fmt.Println("Total :", total)
 }
 
-type Product struct {
+type Utility struct {
 	Name string
 	Price float32
 }
 
-func (product Product) Total(quantity float32) float32 {
-	return product.Price * quantity
+type Item struct {
+	Product Utility
+	Quantity float32
+}
+
+func (item Item) Total() float32 {
+	return item.Product.Price * item.Quantity
 }
 
 func main() {
 	cart := Cart{ 
-		List: map[Product]float32{
-			Product{"arroz", 45.00}: 1.50,
-			Product{"leche", 46.50}: 2.00,
-			Product{"palta", 699.00}: 0.25,
-			Product{"yerba", 120.33}: 3.00,
+		List: []Item{
+			Item{Utility{"arroz", 45.00}, 1.50},
+			Item{Utility{"leche", 46.50}, 2.00},
+			Item{Utility{"palta", 699.00}, 0.25},
+			Item{Utility{"yerba", 120.33}, 3.00},
 		}}
 	cart.Checkout()
 }
