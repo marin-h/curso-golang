c8aa6ff734e56d85bee5d9ac16b87cd36037709f
marin-h <marinahuberman@gmail.com>
2020-07-03 14:43:10 -0300

    round down float representations

--- clase3/main.go
+++ clase3/main.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"math"
 )
 
 type Cart struct {
 	List []Item
 }
 
+func RoundDown (number float64) float64 {
+	return math.Floor(number*100)/100
+}
+
 func (cart Cart) Checkout() {
 	fmt.Println("============== CHECKOUT ==============", "\n")
-	var total float32 
+	var total float64
 	for _, item := range cart.List {
-		totalItem := item.Total()
+		totalItem := RoundDown(item.Total())
 		fmt.Println("*", item.Product.GetName(), "\n", 
 			"	Unit price :", item.Product.GetPrice(), "\n", 
 			"	Quantity :", item.Quantity, "\n", 
-			"	Discount:", 1 - item.Product.GetDiscount(), "%", "\n", 
+			"	Discount:", RoundDown(1 - item.Product.GetDiscount()), "%", "\n", 
 			"	Price with discount:", totalItem, "\n") 
 		total += totalItem
 	}
@@ -27,18 +32,18 @@ func (cart Cart) Checkout() {
 
 type Vegetable struct {
 	Name string
-	Price float32
+	Price float64
 }
 
 type Food struct {
 	Name string
-	Price float32
+	Price float64
 }
 
 type Utility interface {
 	GetName() string
-	GetPrice() float32
-	GetDiscount() float32
+	GetPrice() float64
+	GetDiscount() float64
 }
 
 func (food Food) GetName() string {
@@ -49,15 +54,15 @@ func (vegetable Vegetable) GetName() string {
 	return vegetable.Name
 }
 
-func (food Food) GetPrice() float32 {
+func (food Food) GetPrice() float64 {
 	return food.Price
 }
 
-func (vegetable Vegetable) GetPrice() float32 {
+func (vegetable Vegetable) GetPrice() float64 {
 	return vegetable.Price
 }
 
-func (food Food) GetDiscount() float32 {
+func (food Food) GetDiscount() float64 {
 	if strings.HasPrefix(food.Name, "a") {
 		return 0.7
 	} else {
@@ -65,7 +70,7 @@ func (food Food) GetDiscount() float32 {
 	}
 }
 
-func (vegetable Vegetable) GetDiscount() float32 {
+func (vegetable Vegetable) GetDiscount() float64 {
 	if time.Now().Weekday().String() == "Friday" {
 		return 0.85
 	} else {
@@ -75,11 +80,11 @@ func (vegetable Vegetable) GetDiscount() float32 {
 
 type Item struct {
 	Product Utility
-	Quantity float32
+	Quantity float64
 }
 
-func (item Item) Total() float32 {
-	return item.Product.GetPrice() * item.Product.GetDiscount() * item.Quantity
+func (item Item) Total() float64 {
+	return item.Product.GetPrice() * float64(item.Product.GetDiscount()) * item.Quantity
 }
 
 func main() {
