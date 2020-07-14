ba5baae0be60a3fc45047cdcdcb6c2e499ff728d
marin-h <marinahuberman@gmail.com>
2020-07-13 22:12:46 -0300

    fixing race condition

--- clase4/main.go
+++ clase4/main.go
@@ -14,34 +14,32 @@ func (p product) Price() float64 {
 	return float64(p)
 }
 
-func sumPrice(prod product, total *float64, wg *sync.WaitGroup) {
+func sumPrice(prod product, total *float64, wg *sync.WaitGroup, mu *sync.Mutex) {
 
-	defer func(wg *sync.WaitGroup) {
+	defer func(mu *sync.Mutex, wg *sync.WaitGroup) {
 		wg.Done()
-	}(wg)
+		mu.Unlock()
+	}(mu, wg)
 
- 	*total += prod.Price()
+	price := prod.Price()
+	mu.Lock()
+	*total += price
 }
 
-func calculate(market string, products []product, wg *sync.WaitGroup, result map[string]*float64) {
+func calculate(market string, products []product, wg *sync.WaitGroup, mu *sync.Mutex, result map[string]*float64) {
 
-	var mu sync.Mutex
-	
-	defer func(mu *sync.Mutex, wg *sync.WaitGroup) {
-		mu.Unlock()
-	}(&mu, wg)
-
-	total := result[market]
-
-	mu.Lock()
 	for _, product := range products {
-		go sumPrice(product, total, wg)
+		mu.Lock()
+		total := result[market]
+		mu.Unlock()
+		go sumPrice(product, total, wg, mu)
 	}
 }
 
 func main() {
 
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 
 	var totalChino float64
 	var totalDia float64
@@ -61,7 +59,7 @@ func main() {
 
 	for market, products := range markets {
 		wg.Add(len(products))
-		go calculate(market, products, &wg, result)
+		go calculate(market, products, &wg, &mu, result)
 		wg.Wait()
 		fmt.Println(market, *result[market])
 	}
